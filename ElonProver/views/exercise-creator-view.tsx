66334b9bf398d43049fdc6574c8e2f7e9fd2be5f
rennelou <rennelou@outlook.com>
2023-10-08 17:38:29 -0300

    component proposition-creator-view

--- ElonProver/views/exercise-creator-view.tsx
+++ ElonProver/views/exercise-creator-view.tsx
@@ -1,113 +1,17 @@
 import React, { useState } from 'react';
-import {
-  Text,
-  StyleSheet,
-  View,
-  SafeAreaView,
-  TouchableOpacity,
-  FlatList,
-  TextInput
-} from 'react-native';
+import { View, Button } from 'react-native';
+import PropositionCreatorView from './components/proposition-creator-view';
 
-export default function ExerciseCreatorView() {
-  
-  const [propositions, setPropositions] = useState([]);
-  const [isAddingProposition, setIsAddingProposition] = useState(false);
-  const [propositionText, setPropositionText] = useState('');
+export default function App() {
+  const [propositions, setPropositions] = useState<string[]>([]);
 
-  const startAddingProposition = () => {
-    setIsAddingProposition(true);
-  };
-
-  const confirmProposition = () => {
-    if (propositionText.trim() !== '') {
-      setPropositions([...propositions, propositionText]);
-      setPropositionText('');
-      setIsAddingProposition(false);
-    }
+  const addProposition = (newProposition: string) => {
+    setPropositions([...propositions, newProposition]);
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Insert your proposition</Text>
-        <TouchableOpacity style={styles.addButton} onPress={startAddingProposition}>
-          <Text>Add a new proposition</Text>
-        </TouchableOpacity>
-      </View>
-
-      {isAddingProposition ? (
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            value={propositionText}
-            onChangeText={(text) => setPropositionText(text)}
-            placeholder="Enter your proposition"
-          />
-          <TouchableOpacity style={styles.confirmButton} onPress={confirmProposition}>
-            <Text>Confirm</Text>
-          </TouchableOpacity>
-        </View>
-      ) : null}
-
-      <FlatList
-        data={propositions}
-        renderItem={({ item }) => (
-          <View style={styles.propositionContainer}>
-            <Text>{item}</Text>
-          </View>
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-                 
-    </SafeAreaView>
+    <View>
+      <PropositionCreatorView propositions={propositions} onAddProposition={addProposition} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    marginTop: 50,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginRight: 10, // Espaçamento entre o título e o botão
-  },
-  addButton: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 10,
-    borderRadius: 5,
-  },
-  confirmButton: {
-    backgroundColor: 'green',
-    padding: 10,
-    borderRadius: 5,
-    marginLeft: 10,
-  },
-  propositionContainer: {
-    backgroundColor: 'lightgray',
-    padding: 10,
-    borderRadius: 5,
-    marginVertical: 5,
-  }  
-});
